c8fc5a307d1e8914c06dfa341182380f7bec9039
didoff <dlucky95@gmail.com>
2016-10-25 02:00:28 +0300

    Arr and List initiated + 2 tasks

---
+++ DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input > 0 && input <= 7)
+            {
+                Console.WriteLine(weekdays[input - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+        }
+    }
+}
---
+++ ReverseAnArrayOfIntegeres/ReverseAnArrayOfIntegeres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseAnArrayOfIntegeres
+{
+    class ReverseAnArrayOfIntegeres
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] numbers = new int[input];
+
+
+            for (int i = 0; i < input; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = input - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+           
+        }
+    }
+}
